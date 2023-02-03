ea60c9aa5913d366ee2bac6aebe1d788d392d487
Rahul Patra <rahul.patra@cropsly.com>
2023-02-03 11:23:17 +0530

    added package.swift

---
+++ Package.swift
@@ -0,0 +1,104 @@
+import PackageDescription
+
+let package = Package(
+    name: "MapboxNavigation",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "MapboxCoreNavigation",
+            targets: [
+                "MapboxCoreNavigation",
+            ]
+        ),
+        .library(
+            name: "MapboxNavigation",
+            targets: [
+                "MapboxNavigation",
+            ]
+        )
+    ],
+    dependencies: [
+        .package(name: "MapboxDirections", url: "https://github.com/mapbox/mapbox-directions-swift.git", .exact("2.10.0-beta.1")),
+        .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", from: "1.0.0"),
+        .package(name: "MapboxNavigationNative", url: "https://github.com/mapbox/mapbox-navigation-native-ios.git", from: "124.0.1"),
+        .package(name: "MapboxMaps", url: "https://github.com/mapbox/mapbox-maps-ios.git", .exact("10.11.0-beta.1")),
+        .package(name: "Solar", url: "https://github.com/ceeK/Solar.git", from: "3.0.0"),
+        .package(name: "MapboxSpeech", url: "https://github.com/mapbox/mapbox-speech-swift.git", from: "2.0.0"),
+        .package(name: "CwlPreconditionTesting", url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.1.0"),
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .exact("1.9.0")),
+        .package(name: "OHHTTPStubs", url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "MapboxCoreNavigation",
+            dependencies: [
+                "MapboxDirections",
+                "MapboxMobileEvents",
+                "MapboxNavigationNative",
+            ],
+            exclude: ["Info.plist"],
+            resources: [.copy("Resources/MBXInfo.plist")]),
+        .target(
+            name: "MapboxNavigation",
+            dependencies: [
+                "MapboxCoreNavigation",
+                "MapboxDirections",
+                "MapboxMaps",
+                "MapboxSpeech",
+                "Solar",
+            ],
+            exclude: ["Info.plist"],
+            resources: [.copy("Resources/MBXInfo.plist")]),
+        .target(
+            name: "CTestHelper",
+            dependencies: [
+                "MapboxMobileEvents",
+                "MapboxCoreNavigation",
+            ]),
+        .target(
+            name: "TestHelper",
+            dependencies: [
+                "CTestHelper",
+                "CwlPreconditionTesting",
+                "MapboxCoreNavigation",
+                "MapboxNavigation",
+                "MapboxMaps",
+            ],
+            exclude: ["Info.plist"],
+            resources: [
+                .process("Fixtures"),
+                .process("tiles"),
+            ]
+        ),
+        .target(
+            name: "CarPlayTestHelper",
+            exclude: [
+                "Info.plist",
+                "CarPlayTestHelper.h",
+            ]
+        ),
+        .testTarget(
+            name: "MapboxCoreNavigationTests",
+            dependencies: ["TestHelper"],
+            exclude: ["Info.plist"],
+            resources: [
+                .process("Fixtures"),
+            ]
+        ),
+        .testTarget(
+            name: "MapboxNavigationTests",
+            dependencies: [
+                "MapboxNavigation",
+                "TestHelper",
+                "OHHTTPStubs",
+                "CarPlayTestHelper",
+                "SnapshotTesting",
+            ],
+            exclude: [
+                "Info.plist",
+                "__Snapshots__", // Ignore snapshots folder
+            ]
+        ),
+    ]
+)
